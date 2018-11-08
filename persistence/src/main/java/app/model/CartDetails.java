4fb9975dc2a27564d2c05ca76c5baa5fc738146f
trongtq98 <trongtqk68@gmail.com>
2018-11-08 08:41:07 +0700

    fix init

---
+++ persistence/src/main/java/app/model/CartDetails.java
@@ -0,0 +1,52 @@
+package app.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CartDetails implements java.io.Serializable{
+    private Integer id;
+    private BigDecimal price;
+    private Integer amount;
+    private Cart cart;
+    private List<Book> books;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
---
+++ persistence/src/main/java/app/model/User.java
@@ -0,0 +1,106 @@
+package app.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class User implements java.io.Serializable{
+    private Integer id;
+    private String email;
+    private String password;
+    private String fullName;
+    private String phone;
+    private Boolean gender;
+    private Date birthday;
+    private String role;
+    private List<Address> addresses;
+    private Cart cart;
+    private List<Order> orders;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
